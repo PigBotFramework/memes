b19e5539b0dd5e2fa2cbc0bf3692bb7e9833d811
gingmzmzx <2417481092@qq.com>
2023-04-29 01:30:15 +0800

    定时任务自动push

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
 import time
-from pbf import PBF
-from utils.RegCmd import RegCmd
+from pbf.controller.PBF import PBF
+from pbf.utils.RegCmd import RegCmd
 
 _name = "表情包增强"
 _version = "1.0.1"
@@ -11,46 +11,38 @@ _cost = 0.00
 class memes(PBF):
     def __enter__(self):
         return [
-            RegCmd(
-                name = "添加快捷表情 ",
-                usage = "添加快捷表情 <触发关键词> <图片>",
-                permission = "anyone",
-                function = "memes@add",
-                description = "添加快捷表情",
-                mode = "表情增强",
-                hidden = 0,
-                type = "command"
-            ),
-            RegCmd(
-                name = "MessageListener",
-                usage = "MessageListener",
-                permission = "anyone",
-                function = "memes@messageListener",
-                description = "MessageListener",
-                mode = "表情增强",
-                hidden = 0,
-                type = "message"
-            ),
-            RegCmd(
-                name = "删除快捷表情 ",
-                usage = "删除快捷表情 <关键词>",
-                permission = "anyone",
-                function = "memes@rmMemes",
-                description = "删除快捷表情",
-                mode = "表情增强",
-                hidden = 0,
-                type = "command"
-            ),
-            RegCmd(
-                name = "快捷表情列表",
-                usage = "快捷表情列表",
-                permission = "anyone",
-                function = "memes@listMemes",
-                description = "列出您的快捷表情",
-                mode = "表情增强",
-                hidden = 0,
-                type = "command"
-            )
+    @RegCmd(
+        name = "添加快捷表情 ",
+        usage = "添加快捷表情 <触发关键词> <图片>",
+        permission = "anyone",
+        function = "memes@add",
+        description = "添加快捷表情",
+        mode = "表情增强"
+    )
+    @RegCmd(
+        name = "MessageListener",
+        usage = "MessageListener",
+        permission = "anyone",
+        function = "memes@messageListener",
+        description = "MessageListener",
+        mode = "表情增强"
+    )
+    @RegCmd(
+        name = "删除快捷表情 ",
+        usage = "删除快捷表情 <关键词>",
+        permission = "anyone",
+        function = "memes@rmMemes",
+        description = "删除快捷表情",
+        mode = "表情增强"
+    )
+    @RegCmd(
+        name = "快捷表情列表",
+        usage = "快捷表情列表",
+        permission = "anyone",
+        function = "memes@listMemes",
+        description = "列出您的快捷表情",
+        mode = "表情增强"
+    )
         ]
     
     def add(self):
